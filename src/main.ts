6db5eda24cc130c1cb5fd772a1687bed20a10b15
Valdo <vteixeiraromao@gmail.com>
2023-02-03 10:56:41 +0000

    Added tutorial v1

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,17 @@ WA.onInit().then(() => {
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
 
+        if(!WA.player.state.tutorialDone){
+            WA.ui.modal.openModal({
+                title: "Tutorial",
+                src: 'https://workadventure.github.io/scripting-api-extra/tutorialv1.html',
+                allow: "fullscreen; clipboard-read; clipboard-write",
+                allowApi: true,
+                position: "right",
+            });
+        }
+    }).catch(e => console.error(e));
 }).catch(e => console.error(e));
 
 export {};
