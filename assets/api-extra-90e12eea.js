247c7ad1175be812d89893a5ecb968d5395ea03b
DollyRamella <108594195+DollyRamella@users.noreply.github.com>
2024-03-14 20:50:39 +0000

    Deploying to gh-pages from master @ 4c280ab5af4b7e7b475cd32dc0c0685b772c1943 🚀

--- assets/api-extra-90e12eea.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./init-22028896.js";console.log("Script started successfully");WA.onInit().then(()=>{console.log("Scripting API ready"),console.log("Player tags: ",WA.player.tags),r().then(()=>{console.log("Scripting API Extra ready")}).catch(o=>console.error(o))}).catch(o=>console.error(o));
