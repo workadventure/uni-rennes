d25e1c9772d03ec989b4185e61fcaaf2f58fa9a5
Dollycius <158097486+Dollycius@users.noreply.github.com>
2024-03-26 16:01:15 +0100

    set tiles

---
+++ tilesets/Arbres.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Arbres" tilewidth="32" tileheight="32" tilecount="500" columns="25">
+ <image source="../Asset Blender/SetArbres.png" width="800" height="640"/>
+</tileset>
