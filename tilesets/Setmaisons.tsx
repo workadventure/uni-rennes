f65d36a6173218ce009057407362bc3ea9dc4fe2
DollyRamella <108594195+DollyRamella@users.noreply.github.com>
2024-03-31 11:38:51 +0200

    mise en ligne carte Mukwegue

---
+++ tilesets/Setmaisons.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Setmaisons" tilewidth="32" tileheight="32" tilecount="1968" columns="41">
+ <image source="Setmaisons.png" width="1312" height="1536"/>
+</tileset>
