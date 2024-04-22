1888e1cd3a51be64e8dda30e338f78c86cf1a83d
DollyRamella <108594195+DollyRamella@users.noreply.github.com>
2024-04-22 18:10:18 +0200

    MAJ muk

--- tilesets/Set arbres Mukgwegue.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Set arbres Mukgwegue" tilewidth="32" tileheight="32" tilecount="910" columns="35">
- <transformations hflip="0" vflip="1" rotate="0" preferuntransformed="0"/>
- <image source="SetarbreTileset.png" width="1120" height="832"/>
-</tileset>
--- tilesets/Setarbre1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Setarbre1" tilewidth="32" tileheight="32" tilecount="420" columns="35">
- <image source="../Asset Blender/Setarbre1.png" width="1134" height="396"/>
-</tileset>
