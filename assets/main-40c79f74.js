12350439725208e3482ab6c57419d6063f9758cc
DollyRamella <108594195+DollyRamella@users.noreply.github.com>
2025-10-14 09:35:55 +0000

    Deploying to gh-pages from master @ 49d32d1fe5dadc67cfcdf4274819b8c27ccebcfc 🚀

---
+++ assets/main-40c79f74.js
@@ -0,0 +1,1 @@
+import{b as r}from"./init-7741dda9.js";function e(){console.log("Script started successfully"),WA.onInit().then(()=>{console.log("Scripting API ready"),console.log("Player tags: ",WA.player.tags),r().then(()=>{console.log("Scripting API Extra ready")}).catch(o=>console.error(o))}).catch(o=>console.error(o))}export{e as b};
