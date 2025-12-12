e5005282b4734628bcff7ac20a11372976061afe
DollyRamella <108594195+DollyRamella@users.noreply.github.com>
2025-12-12 11:09:29 +0000

    Deploying to gh-pages from master @ 62e025552cb11ae61bee87f1b65390486dc0b342 🚀

---
+++ assets/main_help_room-105f3d9b.js
@@ -0,0 +1,2 @@
+import{e as r}from"./extra-utilities-d8eb4fc0.js";import"./main_new-2acb2e0a.js";WA.onInit().then(async()=>{let t=await WA.room.getTiledMap(),e;r.setAllExitPrompts("ExitZones","exitPromptUrl","exitPromptMessage",e,t)}).catch(t=>console.error(t));
+//# sourceMappingURL=main_help_room-105f3d9b.js.map
